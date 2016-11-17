111e66668e8846f31c9f0289ac9fe482e152177e
Elvis P. dos Reis <elvisdosreis@gmail.com>
2016-11-17 14:50:46 -0200

    Comit Inicial

---
+++ src/Order/Cart/Cart.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elvis
+ * Date: 17/11/2016
+ * Time: 14:47
+ */
+
+namespace Reis\SteloSdk\Cart;
+
+
+class Cart
+{
+
+}
---
+++ src/Order/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elvis
+ * Date: 17/11/2016
+ * Time: 14:46
+ */
+
+namespace Reis\SteloSdk;
+
+
+class Customer
+{
+
+}
