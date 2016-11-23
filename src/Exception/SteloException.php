4c58f17c94cfa5bd405e3bb56cacf267dbb0da38
Elvis P. dos Reis <elvisdosreis@gmail.com>
2016-11-23 11:33:23 -0200

    Comit Inicial

---
+++ src/Exception/SteloException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elvis
+ * Date: 21/11/2016
+ * Time: 22:38
+ */
+
+namespace Reis\SteloSdk\Exception;
+
+
+class SteloException extends \Exception
+{
+
+}
